144cece9064c4966b5f00c9ec67e5138907f6fe0
Ammon <Harry.Ammon@tpr.gov.uk>
2019-11-19 10:02:19 +0000

    Added some more normal names

--- TPREmployeePayLibrary/SeedData.cs
+++ TPREmployeePayLibrary/SeedData.cs
@@ -11,11 +11,11 @@ namespace TPREmployeePayLibrary
         {
             return new List<PermanentEmployee>
             {
-                new PermanentEmployee("harry0",20000,500, new DateTimeOffset(2019,3,5,0,0,0,TimeSpan.Zero)),
-                new PermanentEmployee("harry1",11111,444, new DateTimeOffset(2019,4,5,0,0,0,TimeSpan.Zero)),
-                new PermanentEmployee("james",22222,333, new DateTimeOffset(2019,5,5,0,0,0,TimeSpan.Zero)),
-                new PermanentEmployee("matt",33333,222, new DateTimeOffset(2019,6,5,0,0,0,TimeSpan.Zero)),
-                new PermanentEmployee("richard",66666,888, new DateTimeOffset(2019,7,5,0,0,0,TimeSpan.Zero)),
+                new PermanentEmployee("Nicole Perkins",20000,500, new DateTimeOffset(2019,3,5,0,0,0,TimeSpan.Zero)),
+                new PermanentEmployee("Margo Bailey",11111,444, new DateTimeOffset(2019,4,5,0,0,0,TimeSpan.Zero)),
+                new PermanentEmployee("Ashton Botwright",22222,333, new DateTimeOffset(2019,5,5,0,0,0,TimeSpan.Zero)),
+                new PermanentEmployee("Suzanne Hunnisett",33333,222, new DateTimeOffset(2019,6,5,0,0,0,TimeSpan.Zero)),
+                new PermanentEmployee("Devin Disney",66666,888, new DateTimeOffset(2019,7,5,0,0,0,TimeSpan.Zero)),
             };
         }
 
@@ -23,11 +23,11 @@ namespace TPREmployeePayLibrary
         {
             return new List<TempEmployee>
             {
-                new TempEmployee("richard0",34, new DateTimeOffset(2019,8,5,0,0,0,TimeSpan.Zero)),
-                new TempEmployee("richard1",10, new DateTimeOffset(2019,9,5,0,0,0,TimeSpan.Zero)),
-                new TempEmployee("harry",34, new DateTimeOffset(2019,10,5,0,0,0,TimeSpan.Zero)),
-                new TempEmployee("samantha",50, new DateTimeOffset(2019,11,5,0,0,0,TimeSpan.Zero)),
-                new TempEmployee("isabel",100, new DateTimeOffset(2019,12,5,0,0,0,TimeSpan.Zero)),
+                new TempEmployee("Seth Cox",34, new DateTimeOffset(2019,8,5,0,0,0,TimeSpan.Zero)),
+                new TempEmployee("Eimhir Lukeson",10, new DateTimeOffset(2019,9,5,0,0,0,TimeSpan.Zero)),
+                new TempEmployee("Patrick Maynard",34, new DateTimeOffset(2019,10,5,0,0,0,TimeSpan.Zero)),
+                new TempEmployee("Duncan Queen",50, new DateTimeOffset(2019,11,5,0,0,0,TimeSpan.Zero)),
+                new TempEmployee("Tilly Duff",100, new DateTimeOffset(2019,12,5,0,0,0,TimeSpan.Zero)),
             };
         }
     }
