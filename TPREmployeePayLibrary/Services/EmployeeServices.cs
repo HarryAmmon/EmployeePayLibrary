25ebba13094425047ce5f06eb55dfa2b1be1d47a
Harry Ammon <harry_a99@hotmail.co.uk>
2019-12-04 10:58:27 +0000

    Revert "Improve calc weeks worked"

--- TPREmployeePayLibrary/Services/EmployeeServices.cs
+++ TPREmployeePayLibrary/Services/EmployeeServices.cs
@@ -9,32 +9,26 @@ namespace TPREmployeePayLibrary.Services
     {
         private readonly ILog _Log = LogManager.GetLogger(typeof(EmployeeServices));
 
-        public double CalcWeeksWorked(DateTimeOffset StartDate, DateTimeOffset EndDate)
+        public EmployeeServices()
         {
+        }
 
-            _Log.Info($"Calculating WeeksWorked with Start Date and End Date");
-            if (StartDate <= EndDate)
+        public double CalcWeeksWorked(DateTimeOffset StartDate, DateTimeOffset? EndDate)
+        {
+            if (EndDate == null) { EndDate = DateTimeOffset.UtcNow.Date; }
+            double weeksWorked;
+            _Log.Info($"Calculating WeeksWorked");
+            if (StartDate >= EndDate)
             {
-                var result = (EndDate - StartDate).TotalDays / 7;
-                _Log.Info($"Calculation successful.");
-                return result;
+                weeksWorked = (StartDate - EndDate).Value.Days / 7;
             }
             else
             {
+                weeksWorked = -1;
                 _Log.Error($"Unable to calculate weeks worked as the start date was the same or later than the end date! StartDate: {StartDate} EndDate: {EndDate}.");
                 throw new Exception();
             }
-        }
-
-        public double CalcWeeksWorked(DateTimeOffset StartDate)
-        {
-            _Log.Info($"Calculating WeeksWorked with Start Date and Todays Date: {DateTimeOffset.Now}");
-            
-            var result = (DateTimeOffset.Now - StartDate).TotalDays / 7;
-            
-            _Log.Info($"Calculation successful.");
-            
-            return result;
+            return weeksWorked;
         }
     }
 }
