bec1abb4f605561d41aef17185d2826f46edb13d
Harry Ammon <harry.ammon@tpr.gov.uk>
2019-12-04 14:40:17 +0000

    Removed unncesary Interfaces

--- TPREmployeePayLibrary/Services/ITempEmployeeServices.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TPREmployeePayLibrary.Services
-{
-    public interface ITempEmployeeServices
-    {
-        decimal CalculateAnnualPay(decimal HourlyRate, double WeeksWorked);
-
-        decimal CalculateHourlyPay(decimal HourlyRate);
-    }
-}
