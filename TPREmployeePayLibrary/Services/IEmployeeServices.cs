8161c84c03298a69c08426f692d57e3b681fb9aa
Harry Ammon <harry.ammon@tpr.gov.uk>
2019-12-11 16:21:09 +0000

    Changed namespace

--- TPREmployeePayLibrary/Services/IEmployeeServices.cs
+++ TPREmployeePayLibrary/Services/IEmployeeServices.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using TPREmployeePayLibrary.Entities;
 
-namespace TPREmployeePayLibrary
+namespace TPREmployeePayLibrary.Services
 {
     public interface IEmployeeServices
     {
